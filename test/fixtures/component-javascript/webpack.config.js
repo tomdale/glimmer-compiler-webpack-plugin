ee5293b458090c4c8142e344faf80190b801c9c8
Thomas Sauer <t-sauer@outlook.de>
2018-01-14 19:55:10 +0100

    Added failing test for entry level component javascript

---
+++ test/fixtures/component-javascript/webpack.config.js
@@ -0,0 +1,33 @@
+require('ts-node/register');
+
+const GlimmerCompiler = require('../../../src');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname),
+  entry: './index.js',
+  target: 'node',
+  node: {
+    __dirname: false
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'commonjs',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /table\.js$/,
+        use: GlimmerCompiler.data()
+      }
+    ]
+  },
+  plugins: [
+    new GlimmerCompiler({
+      output: 'templates.gbx',
+      mode: 'module-unification'
+    }),
+  ]
+}
